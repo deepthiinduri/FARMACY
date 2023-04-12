7d0264ad1dafa6be15a1b7a4039602acdc67250f
INDURI DEEPTHI <68222031+deepthiinduri@users.noreply.github.com>
2023-04-13 00:05:19 +0530

    Update setup.py

--- FARMACY CODE/webapp/setup.py
+++ FARMACY CODE/webapp/setup.py
@@ -5,6 +5,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow
 import numpy as np
 import pickle
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -29,14 +30,25 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/plantdisease/<res>')
-def plantresult(res):
-    print(res)
-    corrected_result = ""
-    for i in res:
-        if i!='_':
-            corrected_result = corrected_result+i
-    return render_template('plantdiseaseresult.html', corrected_result=corrected_result)
+@app.route('/plantdisease/<filenames>/<results>')
+def plantresult(filenames, results):
+    filenames = filenames.split(',')
+    results = results.split(',')
+    corrected_results = []
+    for res in results:
+        corrected_results.append(res.replace('_', ' ').replace(",", "").replace("'", "").replace("[", "").replace("]", ""))
+    img_urls = []
+    for filename in filenames:
+        img_url = url_for('send_uploaded_file', filename=filename.replace(" ", "").replace(",", "").replace("'", "").replace("[", "").replace("]", ""))
+        img_urls.append(img_url)
+    return render_template('plantdiseaseresult.html', corrected_results=corrected_results, img_urls=img_urls)
+
+
+@app.route('/disease-details/<disease_name>')
+def disease_details(disease_name):
+    img_url = request.args.get('img_url')
+    return render_template('diseasedetails.html', disease_name=disease_name, img_url=img_url)
+
 
 @app.route('/plantdisease', methods=['GET', 'POST'])
 def plantdisease():
@@ -44,27 +56,38 @@ def plantdisease():
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            model = MODEL
-            imagefile = tensorflow.keras.utils.load_img(os.path.join(app.config['UPLOAD_FOLDER'], filename), target_size=(224, 224, 3))
-            input_arr = tensorflow.keras.preprocessing.image.img_to_array(imagefile)
-            input_arr = np.array([input_arr])
-            result = model.predict(input_arr)
-            probability_model = tensorflow.keras.Sequential([model, 
-                                         tensorflow.keras.layers.Softmax()])
-            predict = probability_model.predict(input_arr)
-            p = np.argmax(predict[0])
-            res = CLASSES[p]
-            print(res)
-            return redirect(url_for('plantresult', res=res))
+        files = request.files.getlist('file')
+        filenames = []
+        results = []
+        for file in files:
+            # If the user does not select a file, the browser submits an empty file without a filename.
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                filenames.append(filename)
+                model = MODEL
+                imagefile = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                imagefile = imagefile.resize((224, 224))
+                input_arr = tensorflow.keras.preprocessing.image.img_to_array(imagefile)
+                input_arr = np.array([input_arr])
+                result = model.predict(input_arr)
+                probability_model = tensorflow.keras.Sequential([model, 
+                                             tensorflow.keras.layers.Softmax()])
+                predict = probability_model.predict(input_arr)
+                p = np.argmax(predict[0])
+                res = CLASSES[p]
+                results.append(res)
+
+        return redirect(url_for('plantresult', filenames=filenames, results=results))
     return render_template("plantdisease.html")
+
+
+@app.route('/bucket/<filename>')
+def send_uploaded_file(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
 if __name__== "__main__":
